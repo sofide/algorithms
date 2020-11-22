dba33e0d0a84f9e228a51c14080dd4a7bfc908d8
Sofía Denner <sofia.denner@mercadolibre.com>
2020-11-22 18:13:40 -0300

    code style

--- test_c01_w02_counting_inversions.py
+++ test_c01_w02_counting_inversions.py
@@ -3,7 +3,6 @@ import pytest
 from c01_w02_counting_inversions import sort_and_count
 
 
-
 INVERSION_CASES = [
     ([1, 3, 5, 2, 4, 6], (3, [1, 2, 3, 4, 5, 6])),
     ([1, 2, 3], (0, [1, 2, 3])),
@@ -13,4 +12,3 @@ INVERSION_CASES = [
 @pytest.mark.parametrize("input_list,expected_output", INVERSION_CASES)
 def test_sort_and_count(input_list, expected_output):
     assert sort_and_count(input_list) == expected_output
-
