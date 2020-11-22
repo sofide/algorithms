5ab2bd7f8c10d20ab60c480f38d1d80f362976b2
Sofía Denner <sofia.denner@mercadolibre.com>
2020-11-22 11:42:35 -0300

    add docstrings

--- c01_w01_multiply_numbers.py
+++ c01_w01_multiply_numbers.py
@@ -1,6 +1,23 @@
+"""
+Assignment for first week of "Divide and Conquer" course.
+---------------------------------------------------------
+
+In this programming assignment you will implement one or more of the integer
+multiplication algorithms described in lecture.
+
+To get the most out of this assignment, your program should restrict itself to
+multiplying only pairs of single-digit numbers. You can implement the grade-school
+algorithm if you want, but to get the most out of the assignment you'll want to
+implement recursive integer multiplication and/or Karatsuba's algorithm.
+
+So: what's the product of the following two 64-digit numbers?
+
+3141592653589793238462643383279502884197169399375105820974944592
+
+2718281828459045235360287471352662497757247093699959574966967627
+"""
 import math
 import sys
-
 
 
 def multiply_karatsuba(x, y):
@@ -40,3 +57,9 @@ def multiply_karatsuba(x, y):
     result = addend_1 + addend_2 + addend_3
 
     return result
+
+
+if __name__ == "__main__":
+    _, _x, _y = sys.argv
+
+    print(multiply_karatsuba(int(_x), int(_y)))
