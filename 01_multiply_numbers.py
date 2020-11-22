9034702032e90122e012129a3f0d64c851dde9f2
Sofía Denner <sofia.denner@mercadolibre.com>
2020-11-22 11:34:37 -0300

    remove test from homework

--- 01_multiply_numbers.py
+++ 01_multiply_numbers.py
@@ -1,7 +1,6 @@
 import math
 import sys
 
-import pytest
 
 
 def multiply_karatsuba(x, y):
@@ -41,28 +40,3 @@ def multiply_karatsuba(x, y):
     result = addend_1 + addend_2 + addend_3
 
     return result
-
-
-
-@pytest.mark.parametrize("x,y,result", [
-    (20, 1, 20),
-    (88, 27, 2376),
-    (3, 4, 12),
-    (10, 10, 100),
-    (10, 20, 200),
-    (1000, 1000, 1000000),
-    (8888, 2727, 24237576),
-    (272727, 442, 120545334),
-    (2718281828459045235360287471352662497757247093699959574966967627,
-     3141592653589793238462643383279502884197169399375105820974944592,
-     8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184,
-     )
-])
-def test_multiply_karatsuba(x, y, result):
-    assert multiply_karatsuba(x, y) == result
-
-
-if __name__ == "__main__":
-    _, _x, _y = sys.argv
-
-    print(multiply_karatsuba(int(_x), int(_y)))
